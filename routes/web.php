de78a7f3e36498d54eb556121cb236a9e59c88d0
Paul A <impaul@hey.com>
2022-01-07 14:38:31 +0200

    Reproduce the issue.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Database\Eloquent\Model;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,58 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+class Environment extends Model
+{
+    protected static $unguarded = true;
+}
+
+class Deployment extends Model
+{
+    protected static $unguarded = true;
+}
+
+class Project extends Model
+{
+    public function deployments()
+    {
+        return $this->hasManyThrough(Deployment::class, Environment::class);
+    }
+}
+
+
 Route::get('/', function () {
-    return view('welcome');
+    $project = Project::firstOr(function () {
+        $project = Project::create();
+
+        // User with ID 1 creates an environment.
+        $environment = Environment::create([
+            'user_id' => 1,
+            'project_id' => $project->getKey(),
+        ]);
+
+        // User with ID 2 creates a deployment.
+        Deployment::create([
+            'user_id' => 2,
+            'environment_id' => $environment->getKey(),
+        ]);
+
+        return $project;
+    });
+
+    /*
+     * When set to `true`, this will update the select statement
+     * from `SELECT *` to `SELECT deployments.*`
+     * which will prevent from mixing up the values between the intermediate and final tables.
+     */
+    $workaroundEnabled = false;
+
+    $projectDeployments = $project->deployments()
+        ->when($workaroundEnabled, function ($query) {
+            $query->select('deployments.*');
+        })
+        ->cursorPaginate();
+
+    ddd(
+        $projectDeployments->first()->user_id, // Should return 2.
+    );
 });
